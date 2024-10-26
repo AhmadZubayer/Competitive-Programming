da95bafbd63212fc2d470ede25e5fbb274418350
Ahmad Zubayer <ahmadzubayer369@gmail.com>
2024-10-26 10:38:45 +0600

    update

--- Topicwise/Array/Summation CF.cpp
+++ Topicwise/Array/Summation CF.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    if ( 1<= N && N<=100000)  {
+    
         int arr[N];
         long long sum=0; // N can be as large as 10^5,
         for(int i=0; i<N; i++) {
@@ -12,6 +12,6 @@ int main() {
            sum += arr[i]; 
         }
      cout << abs(sum) << endl;
-    }
+    return 0;
     
 }
